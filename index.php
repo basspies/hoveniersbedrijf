e8133e226c37e84e4437e864f83a294091f3dcca
basspies <haroldspies@hotmail.com>
2025-06-20 16:02:04 +0200

    commit

--- index.php
+++ index.php
@@ -3,10 +3,20 @@ include 'includes/header.php';
 ?>
     
 
-<div class = "navbalk">
-    <button>hallo mensen</button>
-</div>
+<div class = "navbar">
+    <img src="img/logo.jpg" alt="bomen" class="logo">
 
+        <button>home</button>
+        <button>contact</button>
+</div>
+    <img src="img/bomen.jpg" alt="bomen" class="bomen">
+
+<div class="diensten">
+    <img src="img/tuinaanleg.jpg" alt="tuinaanleg">
+    <img src="img/tuinonderhoud.jpg" alt="tuinonderhoud">
+    <img src="img/tuinontwerp.jpg" alt="tuinontwerp">
+    <img src="img/zwemvijvers.jpg" alt="zwemvijvers">
+</div>
 
 
 
