f09d8587fcfb2865fec22cf1197ea6602b4469bb
Nielan2008 <nap.ukdh@gmail.com>
2025-06-24 13:31:26 +0200

    test push

--- diensten.php
+++ diensten.php
@@ -4,18 +4,18 @@ include 'includes/header.php';
     
 
 <div class = "navbar">
-    <img src="img/logo.jpg" alt="bomen" class="logo">
+    <img src="assets/img/logo.jpg" alt="bomen" class="logo">
 
         <button>home</button>
         <button>contact</button>
 </div>
-    <img src="img/bomen.jpg" alt="bomen" class="bomen">
+    <img src="assets/img/bomen.jpg" alt="bomen" class="bomen">
 
 <div class="diensten">
-    <img src="img/tuinaanleg.jpg" alt="tuinaanleg">
-    <img src="img/tuinonderhoud.jpg" alt="tuinonderhoud">
-    <img src="img/tuinontwerp.jpg" alt="tuinontwerp">
-    <img src="img/zwemvijvers.jpg" alt="zwemvijvers">
+    <img src="assets/img/tuinaanleg.jpg" alt="tuinaanleg">
+    <img src="assets/img/tuinonderhoud.jpg" alt="tuinonderhoud">
+    <img src="assets/img/tuinontwerp.jpg" alt="tuinontwerp">
+    <img src="assets/img/zwemvijvers.jpg" alt="zwemvijvers">
 </div>
 
 
--- index.php
+++ index.php
@@ -4,12 +4,24 @@ include 'includes/header.php';
     
 
 <div class = "navbar">
-    <img src="img/logo.jpg" alt="bomen" class="logo">
+    <img src="assets/img/logo.jpg" alt="bomen" class="logo">
 
-        <button>home</button>
+        <button>diensten</button>
         <button>contact</button>
 </div>
-    <img src="img/bomen.jpg" alt="bomen" class="bomen">
+<div class="bomen-container">
+    <img src="assets/img/bomen.jpg" alt="bomen" class="bomen">
+<div class="text-wrapper">
+    <h1>Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</h1>
+    <p>Voorbeelden van werk met een slideshow met foto’s.</p>
+</div>
+</div>
+
+<div class="slide_show-container">
+
+
+</div>
+
 
 <?php
 include 'includes/footer.php';
